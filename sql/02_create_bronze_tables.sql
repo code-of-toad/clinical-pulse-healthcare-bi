045f0bc2d5f60d16c67d6dc729e71fbbafbed09a
Toad <codeoftoad@gmail.com>
2026-05-29 19:16:39 -0400

    Create bronze encounter and condition tables AB#1457

--- sql/02_create_bronze_tables.sql
+++ sql/02_create_bronze_tables.sql
@@ -9,6 +9,8 @@
     - bronze.patients
     - bronze.organizations
     - bronze.providers
+    - bronze.encounters
+    - bronze.conditions
 
     Assumptions:
     - These bronze tables are source-preserving staging tables for Synthea CSV data.
@@ -105,5 +107,53 @@ END
 ELSE
 BEGIN
     PRINT 'bronze.providers already exists.';
+END;
+GO
+
+IF OBJECT_ID(N'bronze.encounters', N'U') IS NULL
+BEGIN
+    CREATE TABLE bronze.encounters (
+        source_encounter_id NVARCHAR(100) NULL,
+        encounter_start_datetime NVARCHAR(50) NULL,
+        encounter_stop_datetime NVARCHAR(50) NULL,
+        source_patient_id NVARCHAR(100) NULL,
+        source_organization_id NVARCHAR(100) NULL,
+        source_provider_id NVARCHAR(100) NULL,
+        source_payer_id NVARCHAR(100) NULL,
+        encounter_class NVARCHAR(100) NULL,
+        encounter_code NVARCHAR(100) NULL,
+        encounter_description NVARCHAR(255) NULL,
+        base_encounter_cost NVARCHAR(50) NULL,
+        total_claim_cost NVARCHAR(50) NULL,
+        payer_coverage NVARCHAR(50) NULL,
+        reason_code NVARCHAR(100) NULL,
+        reason_description NVARCHAR(255) NULL
+    );
+
+    PRINT 'Created bronze.encounters.';
+END
+ELSE
+BEGIN
+    PRINT 'bronze.encounters already exists.';
+END;
+GO
+
+IF OBJECT_ID(N'bronze.conditions', N'U') IS NULL
+BEGIN
+    CREATE TABLE bronze.conditions (
+        condition_start_datetime NVARCHAR(50) NULL,
+        condition_stop_datetime NVARCHAR(50) NULL,
+        source_patient_id NVARCHAR(100) NULL,
+        source_encounter_id NVARCHAR(100) NULL,
+        condition_system NVARCHAR(255) NULL,
+        condition_code NVARCHAR(100) NULL,
+        condition_description NVARCHAR(255) NULL
+    );
+
+    PRINT 'Created bronze.conditions.';
+END
+ELSE
+BEGIN
+    PRINT 'bronze.conditions already exists.';
 END;
 GO
