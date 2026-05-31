e13d6f3cae5a2ebf1baebdeec58a1dfdff1d99ed
Toad <codeoftoad@gmail.com>
2026-05-30 23:56:58 -0400

    Write ingestion logs to audit tables AB#1477

--- sql/audit_tables.sql
+++ sql/audit_tables.sql
@@ -21,21 +21,21 @@ IF OBJECT_ID(N'audit.ingestion_batch', N'U') IS NULL
 BEGIN
     CREATE TABLE audit.ingestion_batch (
         ingestion_batch_id BIGINT IDENTITY(1,1) NOT NULL,
-        source_system NVARCHAR(100) NOT NULL,
-        raw_directory NVARCHAR(500) NULL,
-        ingestion_mode NVARCHAR(20) NOT NULL,
-        entity_count INT NOT NULL,
-        started_at DATETIME2(0) NOT NULL
+        source_system      NVARCHAR(100)        NOT NULL,
+        raw_directory      NVARCHAR(500)        NULL,
+        ingestion_mode     NVARCHAR(20)         NOT NULL,
+        entity_count       INT                  NOT NULL,
+        started_at         DATETIME2(0)         NOT NULL
             CONSTRAINT df_ingestion_batch_started_at
             DEFAULT SYSUTCDATETIME(),
-        completed_at DATETIME2(0) NULL,
-        load_status NVARCHAR(30) NOT NULL
+        completed_at       DATETIME2(0)         NULL,
+        load_status        NVARCHAR(30)         NOT NULL
             CONSTRAINT df_ingestion_batch_load_status
             DEFAULT N'running',
-        total_rows_loaded BIGINT NOT NULL
+        total_rows_loaded  BIGINT               NOT NULL
             CONSTRAINT df_ingestion_batch_total_rows_loaded
             DEFAULT (0),
-        error_message NVARCHAR(MAX) NULL,
+        error_message      NVARCHAR(MAX)        NULL,
 
         CONSTRAINT pk_ingestion_batch
             PRIMARY KEY (ingestion_batch_id),
@@ -56,23 +56,23 @@ IF OBJECT_ID(N'audit.ingestion_file_log', N'U') IS NULL
 BEGIN
     CREATE TABLE audit.ingestion_file_log (
         ingestion_file_log_id BIGINT IDENTITY(1,1) NOT NULL,
-        ingestion_batch_id BIGINT NOT NULL,
-        source_file NVARCHAR(255) NOT NULL,
-        target_schema SYSNAME NOT NULL
+        ingestion_batch_id    BIGINT               NOT NULL,
+        source_file           NVARCHAR(255)        NOT NULL,
+        target_schema         SYSNAME              NOT NULL
             CONSTRAINT df_ingestion_file_log_target_schema
             DEFAULT N'bronze',
-        target_table SYSNAME NOT NULL,
-        started_at DATETIME2(0) NOT NULL
+        target_table          SYSNAME              NOT NULL,
+        started_at            DATETIME2(0)         NOT NULL
             CONSTRAINT df_ingestion_file_log_started_at
             DEFAULT SYSUTCDATETIME(),
-        completed_at DATETIME2(0) NULL,
-        load_status NVARCHAR(30) NOT NULL
+        completed_at          DATETIME2(0)         NULL,
+        load_status           NVARCHAR(30)         NOT NULL
             CONSTRAINT df_ingestion_file_log_load_status
             DEFAULT N'running',
-        rows_loaded BIGINT NOT NULL
+        rows_loaded           BIGINT               NOT NULL
             CONSTRAINT df_ingestion_file_log_rows_loaded
             DEFAULT (0),
-        error_message NVARCHAR(MAX) NULL,
+        error_message         NVARCHAR(MAX)        NULL,
 
         CONSTRAINT pk_ingestion_file_log
             PRIMARY KEY (ingestion_file_log_id),
